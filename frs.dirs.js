7c523be7e753e56f9ddff38689f7a392c1d0d638
Implico <bartosz.sak@implico.pl>
2016-06-22 02:32:38 +0200

    comments

--- frs.dirs.js
+++ frs.dirs.js
@@ -1,6 +1,8 @@
 //custom dir definitions
 module.exports = function(dirs, mode) {
 
+  var path = require('path');
+
   switch (mode) {
 
     //invoked after setting src and dist main dir (set custom src/dist dir here; REMEMBER TO UPDATE SOURCE MAP SETTINGS)
@@ -8,8 +10,8 @@ module.exports = function(dirs, mode) {
       //change src directory to source_dir
       // dirs.src.main = dirs.app + 'source_dir/';
 
-      //change dist directory to "../public_html"
-      // dirs.dist.main = dirs.app + '../public_html/';
+      //change dist directory to "../public_html" (remember to normalize parent-related paths)
+      // dirs.dist.main = path.normalize(dirs.app + '../public_html/');
       break;
 
     //change particular src/dist subdirectories
